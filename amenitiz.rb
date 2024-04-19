52f16d7c13ec26a3d3e121a3f71600e3d1c23043
Pelayo de las Heras <pelayodlh@gmail.com>
2024-04-19 12:41:30 +0200

    Cash register working

--- amenitiz.rb
+++ amenitiz.rb
@@ -4,65 +4,68 @@ ITEMS = {
   "CF1": { name: "Coffee", price: 11.23 }
 }
 
+puts "******************************"
+puts "******************************"
+puts "******************************"
+puts "Welcome to our marvellous app!"
+puts "******************************"
+puts "******************************"
+puts "******************************"
+puts ""
+puts "Here are our products:"
 
-  puts "******************************"
-  puts "******************************"
-  puts "Welcome to our marvellous app!"
-  puts "******************************"
-  puts "******************************"
-  puts "Do you want to buy something? If so, please enter the code of the product"
+ITEMS.each do |key, value|
+  puts ""
+  puts " · #{key} = #{value[:name]} - $#{value[:price]}"
+end
 
-    ITEMS.each do |key, value|
-      puts "#{key} = #{value[:name]} - $#{value[:price]}"
-    end
+puts ""
+puts "Do you want to buy something? If so, please enter the code of the product. Right now we have an offer: buy two and get one for free!"
+puts ""
 
-  puts "Text 'several' if you want more than one product, please. Right now we have an offer: buy two and get one for free!"
+basket = Hash.new(0)
+total_price = 0
+
+loop do
   user_choice = gets.chomp
 
   case user_choice
-  when "GR1"
-    puts "Great! It's #{ITEMS[:GR1][:price]}€. Thank you and have a great day!"
-  when "SR1"
-    puts "Great! They are so tasty this year. It's #{ITEMS[:SR1][:price]}€. Thank you and have a great day"
-  when "CF1"
-    puts "Great! Hope you sleep afterwards though. It's #{ITEMS[:CF1][:price]}€. Thank you and have a great day"
-  when "several"
-    puts "Please, state the product"
-    first_several_user_choice = gets.chomp
-    if ["GR1", "SR1", "CF1"].include?(first_several_user_choice)
-      puts "How many?"
-      several_products_choice = gets.chomp.to_i
-      if several_products_choice > 2
-        if first_several_user_choice == "CF1" && several_products_choice >= 3
-          total_price = (several_products_choice * ITEMS[first_several_user_choice.to_sym][:price]) * (2.0 / 3)
-        elsif first_several_user_choice == "SR1" && several_products_choice >= 3
-          total_price = several_products_choice * 4.50
-        else
-          total_price = several_products_choice * ITEMS[first_several_user_choice.to_sym][:price]
-        end
-        puts "You have bought #{several_products_choice} #{ITEMS[first_several_user_choice.to_sym][:name]}. This would be your total price: #{total_price}€. Do you want to pay already? If so, write 'yes'."
-        user_payment = gets.chomp
-          if user_payment == "yes"
-            puts "Thank you for your shopping! See you soon, buddy!"
-          end
-          until user_payment == "yes"
-            puts "Do you want more products? Please, state which one do you want"
-            second_choice = gets.chomp
-            if ["GR1", "SR1", "CF1"].include?(second_choice)
-              puts "How many?"
-              second_several_products_choice = gets.chomp.to_i
-              total_price = several_products_choice * ITEMS[first_several_user_choice.to_sym][:price] + second_several_products_choice * ITEMS[second_choice.to_sym][:price]
-              puts "You have bought #{several_products_choice} #{ITEMS[first_several_user_choice.to_sym][:name]} and #{second_several_products_choice} #{ITEMS[second_choice.to_sym][:name]}. This would be your total price: #{total_price}€.Do you want to pay already? If so, write 'yes'."
-              user_payment = gets.chomp
-                if user_payment == "yes"
-                  puts "Thank you for your shopping! See you soon, buddy!"
-                end
-            end
-          end
-      elsif several_products_choice == 2
-        puts "Clever! You get one for free. Here's the total price: #{ITEMS[first_several_user_choice.to_sym][:price]}"
+  when "GR1", "SR1", "CF1"
+    puts "How many?"
+    product_choice = user_choice.to_sym
+    product_name = ITEMS[product_choice][:name]
+    product_price = ITEMS[product_choice][:price]
+
+    quantity = gets.chomp.to_i
+
+    if quantity > 2
+      if product_choice == :CF1 && quantity >= 3
+        total_price += (quantity * product_price) * (2.0 / 3)
+      elsif product_choice == :SR1 && quantity >= 3
+        total_price += quantity * 4.50
+      else
+        total_price += quantity * product_price
       end
+    elsif quantity == 2
+      puts "Clever! You get one for free."
+      total_price += product_price
     else
-      puts "Sorry, we don't have that product in our warehouse"
+      total_price += product_price
     end
+
+    basket[product_name] += quantity
+
+    puts "You have bought #{quantity} #{product_name}. Your total price so far is: #{total_price}€."
+    puts "If you want to buy something else, write the code of the product: GR1, SR1 or CF1. If you want to pay, just write 'exit'"
+
+  when "exit"
+    break
   end
+end
+
+puts "Your basket contains: "
+basket.each do |product_name, quantity|
+  puts "#{quantity} units of #{product_name}"
+end
+puts "Total price: #{total_price}€"
+puts "Thank you for your shopping! See you soon, buddy!"
